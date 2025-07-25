ddd75a4b18cd3b828e10714295e9d80fda328017
Emily Cowie <emilylcowie@gmail.com>
2025-07-25 21:12:10 +0000

    Welcome Message Done

---
+++ Core Features/Quiz_Core_0.0.py
@@ -0,0 +1,11 @@
+# ######################################
+# Date:             25/07/2025         #
+# Time Started:     22:04              #
+# Time Taken:                          #
+# To Do:            "Welcome Message"  #
+########################################
+
+def Welcome_Message():
+    print('''
+Welcome to this general knowledge quiz!
+          ''')
